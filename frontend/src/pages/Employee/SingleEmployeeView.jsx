f27bf1ddd0b2da158282588792f97713b5904bbc
Crazyshinobi <116379163+Crazyshinobi@users.noreply.github.com>
2024-11-11 17:19:51 +0530

    Update SingleEmployeeView.jsx

--- frontend/src/pages/Employee/SingleEmployeeView.jsx
+++ frontend/src/pages/Employee/SingleEmployeeView.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
 import Cookies from "js-cookie";
 import axios from "axios";
+import defaultUser from "../../assets/defaultUser.png";
 import CircularProgress from "@mui/material/CircularProgress"; // Import CircularProgress
 
 export const SingleEmployeeView = () => {
@@ -141,7 +142,7 @@ export const SingleEmployeeView = () => {
                             </div>
                             <div className="col-span-12 lg:col-span-3 flex flex-col items-center lg:justify-start lg:items-center">
                                 <img
-                                    src={`${process.env.BASE_URL}/uploads${singleEmployee.image}`}
+                                    src={`singleEmployee.image ? ${process.env.BASE_URL}/uploads${singleEmployee.image} : defaultUser`}
                                     alt={singleEmployee.name}
                                     className="lg:h-[250px] lg:w-[250px] h-[180px] w-[180px] rounded-[50%] lg:rounded-lg object-cover object-top mb-5 lg:mb-0"
                                 />
