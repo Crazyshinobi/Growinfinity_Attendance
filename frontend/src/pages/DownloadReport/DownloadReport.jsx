2cf53cc53e80b32286a5cfb6d198065e3204f466
Crazyshinobi <116379163+Crazyshinobi@users.noreply.github.com>
2024-11-11 17:13:33 +0530

    Update DownloadReport.jsx

--- frontend/src/pages/DownloadReport/DownloadReport.jsx
+++ frontend/src/pages/DownloadReport/DownloadReport.jsx
@@ -29,7 +29,7 @@ export const DownloadReport = () => {
     const fetchEmployees = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:4000/api/v1/employee`,
+          `${process.env.BASE_URL}/api/v1/employee`,
           {
             headers: { Authorization: `Bearer ${token}` },
           }
